52525dd458d0fcf0a4336d8b27cdc30fcb138d97
agnieszkaem <agnieszkaem@github.com>
2021-01-04 16:17:32 +0100

    .

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
+const sign_in_button = document.querySelector("#sign-in-button");
+const sign_up_button = document.querySelector("#sign-up-button");
 
 const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener("click", () => {
+sign_up_button.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
 });
 
-sign_in_btn.addEventListener("click", () => {
+sign_in_button.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
